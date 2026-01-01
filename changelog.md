9df2cf5a1715fb28164d847acc11fc5815cae079
Alexandre Caussades <alexcaussades@gmail.com>
2026-01-01 12:14:31 +0100

    Mise à jour de la version dans .env, ajout de la gestion des arguments de ligne de commande dans main.py pour la recherche d'utilisateur et d'email, et mise à jour du changelog.md.

--- changelog.md
+++ changelog.md
@@ -1,4 +1,8 @@
 # Change LOG
 
+- 01/01/2026 
+    - Installation arguments
+    - Mise en place des préférences 
+
 - 30/12/2025 Correctif mineur
 - 28/12/2025 mise en place de l'installation
--- main.py
+++ main.py
@@ -26,6 +26,33 @@ just_fix_windows_console()
 
 load_dotenv()
 
+if (len(sys.argv) > 1 and sys.argv[1] == "--all"):
+    print("Recherche avec toutes les options activées.")
+    option.option_generator()
+    option.ouverture_directory_output()
+    print("Ouverture du répertoire de sortie...")
+    option.fermeture_application()
+    sys.exit()
+elif (len(sys.argv) > 1 and sys.argv[1] == "--help"):
+    print("Utilisation: python main.py [--all] [--help]")
+    print("--all : Recherche avec toutes les options activées.")
+    print("username <username> : Recherche du username spécifié.")
+    print("email <email> : Recherche de l'email spécifié.")
+    print("--help : Affiche ce message d'aide.")
+    sys.exit()
+elif (len(sys.argv) > 1 and sys.argv[1] == "username" or 'u' and sys.argv[2]):
+    username = sys.argv[2]
+    print(f"Recherche du username: {username}")
+    sherlock.search_username_sherlock(username)
+    maigret.search_username_maigret(username)
+    option.ouverture_directory_output()
+    sys.exit()
+elif (len(sys.argv) > 1 and sys.argv[1] == "email" or 'e' and sys.argv[2]):
+    email = sys.argv[2]
+    print(f"Recherche de l'email: {email}")
+    print("Fonction de recherche d'email non encore implémentée.")
+    sys.exit()
+
 option.check_installation_sherlock()
 option.check_installation_maigret()
 print("Vérification de l'installation de Sherlock terminée.")
